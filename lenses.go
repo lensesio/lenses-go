abf1237a84742e2a8d9bb685a84b52d7a384e658
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2018-08-07 14:15:22 +0300

    increment version to 2.1.7

--- lenses.go
+++ lenses.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Version is the current semantic version of the lenses client and cli.
-const Version = "2.1.0"
+const Version = "2.1.7"
 
 // ConnectionOption describes an optional runtime configurator that can be passed on `OpenConnection`.
 // Custom `ConnectionOption` can be used as well, it's just a type of `func(*lenses.Client)`.
