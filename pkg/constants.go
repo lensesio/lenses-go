6d3d015b3ee914ab5f541264236eed73fbb44cc5
Marcin Piątek <marcin.piatek@lenses.io>
2021-01-27 14:04:52 +0100

    Fix `alerts` command
    `alerts` command (without `live` flag) was using wrong API endpoint, which makes the output always empty.
    This fix changes the endpoint used by `alerts` command to `api/v1/alert/events`.
    
    Issue: https://landoop.atlassian.net/browse/AC-1346

--- pkg/constants.go
+++ pkg/constants.go
@@ -36,6 +36,7 @@ const (
 	AuditChannelTemplatesPath  = "api/v1/audit/channel-templates"
 	AlertsSettingsPath         = "api/v1/alert/settings"
 	AlertsPathSSE              = "api/sse/alerts"
+	AlertEventsPath            = "api/v1/alert/events"
 	MetadataTopicsPath         = "api/v1/metadata/topics"
 
 	LicensePath = "api/v1/license"
