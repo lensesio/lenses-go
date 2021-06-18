f2f97c6b21d79bb1dc36b8bbccfe60262e44e333
Nicolò Martini <nicolo@martini.io>
2021-06-18 10:23:34 +0200

    NGN-1211: Make topic names list decoding compatible with 4.3 format

--- pkg/api/client.go
+++ pkg/api/client.go
@@ -1550,9 +1550,9 @@ type (
 
 		SQL string `json:"sql"` // header:"SQL"`
 
-		InputTopics  []TopicDecoders `json:"inputTopics"`
-		OutputTopics []TopicDecoders `json:"outputTopics"` // header:"Topic Decoder"`
-		Pipeline     string          `json:"pipeline"`     // header:"Pipeline"`
+		InputTopics  []TopicName `json:"inputTopics"`
+		OutputTopics []TopicName `json:"outputTopics"` // header:"Topic Decoder"`
+		Pipeline     string      `json:"pipeline"`     // header:"Pipeline"`
 
 		ToTopics   []string            `json:"toTopics,omitempty"` // header:"To Topics"`
 		FromTopics []string            `json:"fromTopics,omitempty"`
@@ -1563,11 +1563,16 @@ type (
 	}
 
 	// TopicDecoders contains the information about the topic storage format
-	TopicDecoders struct {
+	TopicName struct {
+		Name string `json:"name"`
+	}
+
+	LegacyTopicName struct {
 		Name  string `json:"name"`
 		Key   string `json:"key"`
 		Value string `json:"value"`
 	}
+
 	// ProcessorLastAction contains the information about the last change on the SQL processor
 	ProcessorLastAction struct {
 		Action  string `json:"action"`
@@ -1590,6 +1595,28 @@ type (
 		ErrorMessage string `json:"errorMsg,omitempty"`
 	}
 )
+
+/**
+ * In Lenses 4.3 the API changed the format of the list of input/output topics.
+ * This Unmarshaler is compatible with both formats
+ */
+func (topicName *TopicName) UnmarshalJSON(data []byte) error {
+
+	var topicString string
+	if err := json.Unmarshal(data, &topicString); err != nil {
+		var legacyTopicName LegacyTopicName
+
+		if err2 := json.Unmarshal(data, &legacyTopicName); err2 != nil {
+			return err2
+		}
+
+		topicString = legacyTopicName.Name
+	}
+
+	topicName.Name = topicString
+
+	return nil
+}
 
 // GetProcessors returns a list of all available LSQL processors.
 func (c *Client) GetProcessors() (ProcessorsResult, error) {
